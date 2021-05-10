2f6df919a164b92d0b409a5989d15c4ec5fc496e
Ali Turki <ali@turkii.me>
2021-05-10 04:10:15 +0200

    feat: implement application class

--- services/backend/src/Application.ts
+++ services/backend/src/Application.ts
@@ -1,9 +1,31 @@
+/* eslint-disable */
 import debug from 'debug';
 import express from 'express';
 import * as http from 'http';
-import bodyParser from 'body-parser';
-import { ApolloServer, CorsOptions } from 'apollo-server';
+import { ApolloServer, CorsOptions, IResolvers } from 'apollo-server';
 import { ApolloServerExpressConfig } from 'apollo-server-express';
+import { GraphQLSchema } from 'graphql';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { sync as readdirSync } from 'glob';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import Database from './utils/database/database';
+import { exec } from 'child_process'
+const log = debug("app:shell")
+const shell = async (cmd: string) => {
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      log(error.message);
+      return;
+    }
+    if (stderr) {
+      log(stderr);
+      return;
+    }
+    log(stdout);
+
+  })
+}
 
 interface GraphQLConfig {
   port: number;
@@ -12,6 +34,7 @@ export interface ApplicationOptions {
   rootDir: string;
   port?: number;
   graphqlConfig?: GraphQLConfig;
+  resolvers?: Array<any>;
 }
 
 export type ApolloServerConfig = ApolloServerExpressConfig & {
@@ -24,8 +47,14 @@ export class Application {
   // Set instance of Debug module
   private log: debug.Debugger = debug('app:Application');
 
+  // resolvers
+  private resolvers: Array<any>;
+
   // Root directory of the project
   private rootDir: string;
+
+  // private database
+  private database: Database
 
   // Express Applicatin instance
   public app: express.Application = express();
@@ -37,7 +66,7 @@ export class Application {
   private apolloServer!: ApolloServer;
 
   // Constant Port
-  private readonly DEFAULT_PORT: number = ((process.env.PORT as unknown) as number) || 3043;
+  private readonly DEFAULT_PORT: number = ((process.env.PORT as unknown) as number) || 3001;
 
   // application port
   private port: number;
@@ -45,20 +74,69 @@ export class Application {
   // Graphql Config
   private gqlConfig: GraphQLConfig;
 
-  constructor({ rootDir, port, graphqlConfig }: ApplicationOptions) {
+  constructor({ rootDir, port, graphqlConfig, resolvers }: ApplicationOptions) {
     this.rootDir = rootDir || __dirname;
     this.port = port || this.DEFAULT_PORT;
-    this.gqlConfig = { port: 4000, ...(graphqlConfig || {}) };
+    this.gqlConfig = { port: 3000, ...(graphqlConfig || {}) };
+    this.resolvers = resolvers || [];
+    this.database = Database.createConnection({})
   }
 
-  private setupMiddlewares() {
-    this.app.use(bodyParser.json());
-    this.app.use(bodyParser.urlencoded({ extended: true }));
+  private async connectToDatabase() {
+    try {
+      await this.database.connect()
+      this.log('Database connection has beend established succefully');
+      // Execuating migration
+
+    } catch (error) {
+      this.log('Something went wrong while connecting to the database.')
+      this.log(error);
+    }
+  }
+
+
+  // setup graphql schema
+  generateGraphQlSchema(): GraphQLSchema {
+    const gqlFiles: Array<string> = readdirSync(join(this.rootDir, '**/*.graphql'));
+    let typeDefs = '';
+    const resolvers = this.loadReslovers();
+    gqlFiles.forEach((file) => {
+      typeDefs += readFileSync(file, {
+        encoding: 'utf8',
+      });
+    });
+    return makeExecutableSchema({
+      typeDefs,
+      resolvers,
+    });
+  }
+
+  // load resolvers
+  loadReslovers(): IResolvers {
+    const Mutation: Record<string, any> = {};
+    const Query: Record<string, any> = {};
+    this.resolvers.forEach((Reslover: any) => {
+      const instance = new Reslover();
+      // load mutations form reslover
+      const mutations = (Reflect.getMetadata('mutations', Reslover) || []) as Array<string>;
+      mutations.forEach((key: string) => {
+        Mutation[key] = instance[key as keyof typeof instance].bind(instance);
+      });
+      // load queries form reslover
+      const queries = (Reflect.getMetadata('queries', Reslover) || []) as Array<string>;
+      queries.forEach((key: string) => {
+        Query[key] = instance[key as keyof typeof instance].bind(instance);
+      });
+    });
+    return {
+      Mutation,
+      Query,
+    };
   }
 
   // Setup Apollo Server for GraphQL
-  public createApolloServer(config: ApolloServerConfig): Application {
-    this.apolloServer = new ApolloServer(config);
+  public createApolloServer(): Application {
+    this.apolloServer = new ApolloServer({ schema: this.generateGraphQlSchema() });
     return this;
   }
 
@@ -76,9 +154,13 @@ export class Application {
 
   // Initialize an run servers
   public listen(callback?: (port: number) => void): void {
+    this.log('Running migration')
+    // Execute mgiration before running the server
+    shell('yarn typeorm migration:run')
     this.httpServer.listen(this.port, () => {
       this.log(`Application up and running on http://localhost:${this.port}`);
       this.log('Starting Apollo Server.');
+      this.connectToDatabase()
       // Run apollo-server only if it is created
       if (this.apolloServer) {
         this.runApolloServer();
